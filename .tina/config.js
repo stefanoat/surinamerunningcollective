a28a7e88ee0c3db6eeb9452d1feb32bd548a6e00
ramiro <ramiroparraperez@gmail.com>
2023-02-25 06:04:05 -0300

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -47,8 +47,8 @@ const about_fields2 = [
 
 export default defineConfig({
   branch,
-  clientId: "90de3673-afce-4ddc-a628-a467a865f688", // Get this from tina.io
-  token: "98ffbcf689f05b834c89e2fb6aba2736034f74a2", // Get this from tina.io
+  clientId: "0e20ddc0-7b7f-4bb8-876e-153c551c5ed5", // Get this from tina.io
+  token: "38e3312db5b2d68921d6ec089ba0ad882e6f1ebd", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
