1a65c45f41f91352af5f1936bd3bf8d8127d51ac
ramiro <ramiroparraperez@gmail.com>
2023-02-25 06:35:17 -0300

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -1,49 +1,7 @@
 import { defineConfig } from "tinacms";
-
+import homePage from "./collections/home.js";
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-
-const fields = [
-  {
-    name: "title",
-    label: "Title",
-    type: "string",  
-  },
-  {
-    name: "idee",
-    label: "Id",
-    type: "string",  
-  },
-  {
-    name: "text_color",
-    label: "Text Color",
-    type: "string",  
-  },
-  {
-    name: "background_color",
-    label: "Background Color",
-    type: "string",  
-  },
-];
-
-
-const about_fields = [
-  {
-    name: 'content',
-    label: 'Content', 
-    type: 'string',
-  },
-];
-
-const about_fields2 = [
-  {
-    name: 'content',
-    label: 'Content', 
-    type: 'string',
-  },
-];
-
-
 
 export default defineConfig({
   branch,
@@ -60,36 +18,8 @@ export default defineConfig({
     },
   },
   schema: {
-    collections: [{
-        label: 'Home Page',
-        name: 'home',
-        path: 'data/',
-        format: 'json',
-        fields: [
-          {
-            label: "Page Blocks",
-            name: "pageBlocks",
-            type: "object",
-            list: true,
-            templates: [
-            {
-              name: 'about',
-              label: 'About',
-              fields: about_fields,
-            },
-            {
-              name: 'banner',
-              label: 'Banner',
-              fields: about_fields2,
-            },
-            ],
-          },
-          {
-            name: 'content',
-            label: 'Content', 
-            type: 'string',
-          },
-        ],
-    }],
+    collections: [
+      homePage
+    ],
   },
 });
