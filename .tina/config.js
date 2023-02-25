1ba6f6d2841a024a2b2914eaf695dc547ee5a0ea
ramiro <ramiroparraperez@gmail.com>
2023-02-25 04:55:20 -0300

    sheet

--- .tina/config.js
+++ .tina/config.js
@@ -47,8 +47,8 @@ const about_fields2 = [
 
 export default defineConfig({
   branch,
-  clientId: "5106844b-dc8e-454a-bbad-9bba15ad6729", // Get this from tina.io
-  token: "5d89d1ee065c445686748f0da5222f0df79b214e", // Get this from tina.io
+  clientId: process.env.clientId, // Get this from tina.io
+  token: process.env.token, // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -70,7 +70,7 @@ export default defineConfig({
             label: "Page Blocks",
             name: "pageBlocks",
             type: "object",
-            // list: true,
+            list: true,
             templates: [
             {
               name: 'about',
